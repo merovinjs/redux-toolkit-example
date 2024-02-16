44a00c3b1520bf824f2831940b90de1d896dd710
Proprogam <k.kartal9595@gmail.com>
2024-02-16 13:43:29 +0300

    fixed:any problem

--- src/redux/dataSlice.ts
+++ src/redux/dataSlice.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
 
@@ -19,25 +20,13 @@ export const dataSlice = createSlice({
       state.data = [...state.data, action.payload];
     },
     sortDataFunc: (state, action: PayloadAction<any>) => {
-      state.data = [
-        ...state.data.sort((a, b) =>
-          action.payload == "asc"
-            ? a.price - b.price
-            : action.payload == "desc"
-            ? b.price - a.price
-            : b.price - a.price
-        ),
-      ];
+      state.data = [...state.data.sort((a, b) => (action.payload == "asc" ? a.price - b.price : action.payload == "desc" ? b.price - a.price : b.price - a.price))];
     },
     deleteDataFunc: (state, action: PayloadAction<any>) => {
       state.data = [...state.data.filter((item) => item.id !== action.payload)];
     },
     updateDataFunc: (state, action: PayloadAction<any>) => {
-      state.data = [
-        ...state.data.map((item) =>
-          item.id == action.payload.id ? { ...item, ...action.payload } : item
-        ),
-      ];
+      state.data = [...state.data.map((item) => (item.id == action.payload.id ? { ...item, ...action.payload } : item))];
     },
     searchDataFunc: (state, action: PayloadAction<any>) => {
       state.keyword = action.payload;
@@ -46,12 +35,6 @@ export const dataSlice = createSlice({
 });
 
 // Action creators are generated for each case reducer function
-export const {
-  createDataFunc,
-  deleteDataFunc,
-  updateDataFunc,
-  sortDataFunc,
-  searchDataFunc,
-} = dataSlice.actions;
+export const { createDataFunc, deleteDataFunc, updateDataFunc, sortDataFunc, searchDataFunc } = dataSlice.actions;
 
 export default dataSlice.reducer;
